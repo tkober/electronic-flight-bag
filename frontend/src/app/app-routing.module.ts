41f6ab6bc165f8666d6c504240884a4b767a37db
Thorsten Kober <thorsten.kober@softgarden.de>
2020-11-22 15:20:08 +0100

    Fixed typo in routing and tslint errors

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {AircraftSelectionComponent} from "./aircraft-selection/aircraft-selection.component";
-import {ElectronicFlightBagComponent} from "./electronic-flight-bag/electronic-flight-bag.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AircraftSelectionComponent} from './aircraft-selection/aircraft-selection.component';
+import {ElectronicFlightBagComponent} from './electronic-flight-bag/electronic-flight-bag.component';
 
 
 const routes: Routes = [
     { path: 'aircraft', component: AircraftSelectionComponent },
     { path: '', redirectTo: '/aircraft', pathMatch: 'full' },
 
-    { path: 'electroni-flight-back', component: ElectronicFlightBagComponent },
+    { path: 'electronic-flight-bag', component: ElectronicFlightBagComponent },
 
     { path: '**', component: PageNotFoundComponent}
 ];
