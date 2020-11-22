ac359c1eb132cf564a9211e524bd84c6b4286c1a
Thorsten Kober <thorsten.kober@softgarden.de>
2020-11-22 15:44:12 +0100

    Some wireframing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AircraftSelectionComponent } from './aircraft-selection/aircraft-selection.component';
-import { ElectronicFlightBagComponent } from './electronic-flight-bag/electronic-flight-bag.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AircraftSelectionComponent} from './aircraft-selection/aircraft-selection.component';
+import {ElectronicFlightBagComponent} from './electronic-flight-bag/electronic-flight-bag.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -15,11 +19,15 @@ import { ElectronicFlightBagComponent } from './electronic-flight-bag/electronic
     AircraftSelectionComponent,
     ElectronicFlightBagComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
